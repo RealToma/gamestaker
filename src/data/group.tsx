9d6b4102ad4efb8ac1c19f4852bae185f707425b
RealToma <real.toma.contact@gmail.com>
2024-06-18 11:00:37 -0400

    updated data table

--- src/data/group.tsx
+++ src/data/group.tsx
@@ -1,111 +1,212 @@
+// export const dataBetGroupList = [
+//   {
+//     groupName: "General Bets",
+//     groupBets: [
+//       {
+//         groupBetName: "Winner",
+//         betBefore: "",
+//         options: [
+//           {
+//             optionName: "Germany",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Scotland",
+//             ratio: 3.6,
+//           },
+//           {
+//             optionName: "Hungary",
+//             ratio: 5.3,
+//           },
+//           {
+//             optionName: "Switzerland",
+//             ratio: 3.3,
+//           },
+//           {
+//             optionName: "Spain",
+//             ratio: 1.6,
+//           },
+//           {
+//             optionName: "Croatia",
+//             ratio: 2.1,
+//           },
+//           {
+//             optionName: "Italy",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Albania",
+//             ratio: 6.3,
+//           },
+//           {
+//             optionName: "Slovenia",
+//             ratio: 4.5,
+//           },
+//           {
+//             optionName: "Denmark",
+//             ratio: 3.6,
+//           },
+//           {
+//             optionName: "Serbia",
+//             ratio: 2.3,
+//           },
+//           {
+//             optionName: "England",
+//             ratio: 1.5,
+//           },
+//           {
+//             optionName: "Poland",
+//             ratio: 2.6,
+//           },
+//           {
+//             optionName: "Netherlands",
+//             ratio: 1.6,
+//           },
+//           {
+//             optionName: "Austria",
+//             ratio: 2.8,
+//           },
+//           {
+//             optionName: "France",
+//             ratio: 1.3,
+//           },
+//           {
+//             optionName: "Belgium",
+//             ratio: 2.3,
+//           },
+//           {
+//             optionName: "Slovakia",
+//             ratio: 3.3,
+//           },
+//           {
+//             optionName: "Romania",
+//             ratio: 4.7,
+//           },
+//           {
+//             optionName: "Ukraine",
+//             ratio: 2.3,
+//           },
+//           {
+//             optionName: "Turkey",
+//             ratio: 2.3,
+//           },
+//           {
+//             optionName: "Georgia",
+//             ratio: 6.7,
+//           },
+//           {
+//             optionName: "Portugal",
+//             ratio: 2.3,
+//           },
+//           {
+//             optionName: "Czechia",
+//             ratio: 2.5,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     groupName: "Group A",
+//     groupBets: [
+//       {
+//         groupBetName: "Germany vs Scotland",
+//         betBefore: "14th June 21:00 CET",
+//         options: [
+//           {
+//             optionName: "Local wins (1)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Visitant wins (2)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Tie (x)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Exact Score",
+//             ratio: 1.2,
+//           },
+//         ],
+//       },
+//       {
+//         groupBetName: "Hungary vs Switzertland",
+//         betBefore: "15th June 15:00 CET",
+//         options: [
+//           {
+//             optionName: "Local wins (1)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Visitant wins (2)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Tie (x)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Exact Score",
+//             ratio: 1.2,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     groupName: "Group B",
+//     groupBets: [
+//       {
+//         groupBetName: "Spain vs Croatia",
+//         betBefore: "15th June 18:00 CET",
+//         options: [
+//           {
+//             optionName: "Local wins (1)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Visitant wins (2)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Tie (x)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Exact Score",
+//             ratio: 1.2,
+//           },
+//         ],
+//       },
+//       {
+//         groupBetName: "Italy vs Albania",
+//         betBefore: "16th June 20:00 CET",
+//         options: [
+//           {
+//             optionName: "Local wins (1)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Visitant wins (2)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Tie (x)",
+//             ratio: 1.2,
+//           },
+//           {
+//             optionName: "Exact Score",
+//             ratio: 1.2,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
 export const dataBetGroupList = [
-  {
-    groupName: "General Bets",
-    groupBets: [
-      {
-        groupBetName: "Winner",
-        betBefore: "",
-        options: [
-          {
-            optionName: "Germany",
-            ratio: 1.2,
-          },
-          {
-            optionName: "Scotland",
-            ratio: 3.6,
-          },
-          {
-            optionName: "Hungary",
-            ratio: 5.3,
-          },
-          {
-            optionName: "Switzerland",
-            ratio: 3.3,
-          },
-          {
-            optionName: "Spain",
-            ratio: 1.6,
-          },
-          {
-            optionName: "Croatia",
-            ratio: 2.1,
-          },
-          {
-            optionName: "Italy",
-            ratio: 1.2,
-          },
-          {
-            optionName: "Albania",
-            ratio: 6.3,
-          },
-          {
-            optionName: "Slovenia",
-            ratio: 4.5,
-          },
-          {
-            optionName: "Denmark",
-            ratio: 3.6,
-          },
-          {
-            optionName: "Serbia",
-            ratio: 2.3,
-          },
-          {
-            optionName: "England",
-            ratio: 1.5,
-          },
-          {
-            optionName: "Poland",
-            ratio: 2.6,
-          },
-          {
-            optionName: "Netherlands",
-            ratio: 1.6,
-          },
-          {
-            optionName: "Austria",
-            ratio: 2.8,
-          },
-          {
-            optionName: "France",
-            ratio: 1.3,
-          },
-          {
-            optionName: "Belgium",
-            ratio: 2.3,
-          },
-          {
-            optionName: "Slovakia",
-            ratio: 3.3,
-          },
-          {
-            optionName: "Romania",
-            ratio: 4.7,
-          },
-          {
-            optionName: "Ukraine",
-            ratio: 2.3,
-          },
-          {
-            optionName: "Turkey",
-            ratio: 2.3,
-          },
-          {
-            optionName: "Georgia",
-            ratio: 6.7,
-          },
-          {
-            optionName: "Portugal",
-            ratio: 2.3,
-          },
-          {
-            optionName: "Czechia",
-            ratio: 2.5,
-          },
-        ],
-      },
-    ],
-  },
   {
     groupName: "Group A",
     groupBets: [
@@ -125,10 +226,6 @@ export const dataBetGroupList = [
             optionName: "Tie (x)",
             ratio: 1.2,
           },
-          {
-            optionName: "Exact Score",
-            ratio: 1.2,
-          },
         ],
       },
       {
@@ -145,10 +242,6 @@ export const dataBetGroupList = [
           },
           {
             optionName: "Tie (x)",
-            ratio: 1.2,
-          },
-          {
-            optionName: "Exact Score",
             ratio: 1.2,
           },
         ],
@@ -174,10 +267,6 @@ export const dataBetGroupList = [
             optionName: "Tie (x)",
             ratio: 1.2,
           },
-          {
-            optionName: "Exact Score",
-            ratio: 1.2,
-          },
         ],
       },
       {
@@ -194,10 +283,6 @@ export const dataBetGroupList = [
           },
           {
             optionName: "Tie (x)",
-            ratio: 1.2,
-          },
-          {
-            optionName: "Exact Score",
             ratio: 1.2,
           },
         ],
